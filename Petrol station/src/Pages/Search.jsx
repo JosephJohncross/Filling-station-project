0499a7dabcd06a0a4578604e54bc09c8a5eda3c5
danutibe07 <danielutibe07@gmail.com>
2023-07-27 03:15:42 +0100

    Search page

--- Petrol station/src/Pages/Search.jsx
+++ Petrol station/src/Pages/Search.jsx
@@ -1,65 +1,144 @@
 import React from "react";
+import StationSearchCard from "../Component/Station/StationSearchCard";
+import Button from "../Component/Common/Button";
 
 const Search = () => {
   return (
-    <div className="container_limiter flex flex-row">
-      <div className="Left w-2/3">
+    <div className="container_limiter flex flex-col font-open">
+      <div className="">
         <form className="my-5 grid gap-7">
-          <h1 className="text-3xl font-bold">Search For Fuel</h1>
-          <div class="relative">
-            <input
-              type="text"
-              id="search"
-              class="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-              placeholder=" "
-            />
-            <label
-              for="search"
-              class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
-            >
-              Search by Name, Street, Area, any key word...
-            </label>
-          </div>
-          <div className="flex flex-row">
-            <div class="relative w-1/2 mr-8">
+          <div className="grid md:grid-cols-4 gap-4 grid-cols-1">
+            <div class="relative">
               <select
-                type="text"
-                id="anytype"
-                class="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                placeholder=" "
+                id="selectField"
+                name="selectField"
+                class="w-full px-3 py-3 border-1 rounded-md focus:outline-none focus:border-blue-600 border-blue-600 text-blue-600 text-sm"
               >
-                <option value="use">Any Types</option>
-                <option value="use">use</option>
+                <option value="option1" default>
+                  Any Types
+                </option>
+                <option value="option1">Petrol</option>
+                <option value="option2">Diesel</option>
+                <option value="option3">Kerosene</option>
               </select>
               <label
-                for="anytype"
-                class="absolute text-lg text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
+                for="outlined_success"
+                class="absolute text-sm text-blue-600 dark:text-green-500 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
               >
-                Floating outlined
+                Commodity
               </label>
             </div>
-            <div class="relative w-1/2 ml-8">
+            <div class="relative">
               <select
-                type="text"
-                id="type1"
-                class="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                placeholder=" "
+                id="selectField"
+                name="selectField"
+                class="w-full px-3 py-3 border-1 rounded-md focus:outline-none focus:border-blue-600 border-blue-600 text-blue-600 text-sm"
               >
-                <option value="use">Any Types</option>
-                <option value="use">use</option>
+                <option value="option1" default>
+                  Random
+                </option>
+                <option value="option1">Cheapest-Expensive</option>
+                <option value="option2">Expensive-Cheapest</option>
               </select>
               <label
-                for="type1"
-                class="absolute text-lg text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
+                for="outlined_success"
+                class="absolute text-sm text-blue-600 dark:text-green-500 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
               >
-                Floating outlined
+                Price
+              </label>
+            </div>
+            <div class="relative">
+              <select
+                id="selectField"
+                name="selectField"
+                class="w-full px-3 py-3 border-1 rounded-md focus:outline-none focus:border-blue-600 border-blue-600 text-blue-600 text-sm"
+              >
+                <option value="option1" default>
+                  Any Time
+                </option>
+                <option value="option1">Open Now</option>
+                <option value="option2">Open 24 Hours</option>
+              </select>
+              <label
+                for="outlined_success"
+                class="absolute text-sm text-blue-600 dark:text-green-500 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
+              >
+                Opening Hours
+              </label>
+            </div>
+            <div class="relative">
+              <select
+                id="selectField"
+                name="selectField"
+                class="w-full px-3 py-3 border-1 rounded-md focus:outline-none focus:border-blue-600 border-blue-600 text-blue-600 text-sm"
+              >
+                <option value="option1" default>
+                  Random
+                </option>
+                <option value="option1">Highest-lowest</option>
+                <option value="option2">Lowest-Highest</option>
+              </select>
+              <label
+                for="outlined_success"
+                class="absolute text-sm text-blue-600 dark:text-green-500 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
+              >
+                Ratings
               </label>
             </div>
           </div>
         </form>
       </div>
-
-      <div>Right</div>
+      <div className="mt-5 overflow-y-hidden flex flex-col gap-y-4 p-3">
+        <h1 className="text-3xl font-bold font-pt">
+          All Filling Stations along Oron Road
+        </h1>
+        <div className="grid md:grid-cols-2 gap-4 grid-cols-1">
+          <StationSearchCard
+            address={"Opposite Timber market Mbierebe."}
+            dieselPrice={""}
+            distance={"1.1 mi"}
+            kerosinePrice={""}
+            name={"Earthwell Filling Station"}
+            openingTime={"8am-9pm"}
+            petrolPrice={""}
+            rating={"4"}
+          />
+          <StationSearchCard
+            address={"Opposite Timber market Mbierebe."}
+            dieselPrice={""}
+            distance={"1.1 mi"}
+            kerosinePrice={""}
+            name={"Earthwell Filling Station"}
+            openingTime={"8am-9pm"}
+            petrolPrice={""}
+            rating={"4"}
+          />
+          <StationSearchCard
+            address={"Opposite Timber market Mbierebe."}
+            dieselPrice={""}
+            distance={"1.1 mi"}
+            kerosinePrice={""}
+            name={"Earthwell Filling Station"}
+            openingTime={"8am-9pm"}
+            petrolPrice={""}
+            rating={"4"}
+          />
+          <StationSearchCard
+            address={"Opposite Timber market Mbierebe."}
+            dieselPrice={""}
+            distance={"1.1 mi"}
+            kerosinePrice={""}
+            name={"Earthwell Filling Station"}
+            openingTime={"8am-9pm"}
+            petrolPrice={""}
+            rating={"4"}
+          />
+        </div>
+        <div className="flex flex-row justify-between">
+          <Button clickFunction={() => {}} shade="white" content="Go Back" />
+          <Button clickFunction={() => {}} shade="blue" content="See More" />
+        </div>
+      </div>
     </div>
   );
 };
