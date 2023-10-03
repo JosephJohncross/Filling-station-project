c0977f9b11d5edf8ae72c71da8f2b69c0d3f22ce
JosephJohncross <josephibochi@gmail.com>
2023-10-03 22:01:35 +0100

    updates

--- Petrol station/src/Component/Common/Drawer.jsx
+++ Petrol station/src/Component/Common/Drawer.jsx
@@ -13,9 +13,13 @@ const Drawer = ({ content }) => {
       <button
         type="button"
         className="text-primColor hover:text-primColor/80 pr-2"
-        data-hs-overlay="#docs-sidebar"
-        aria-controls="docs-sidebar"
-        aria-label="Toggle navigation"
+        // data-hs-overlay="#docs-sidebar"
+        // aria-controls="docs-sidebar"
+        // aria-label="Toggle navigation"
+        data-te-sidenav-toggle-ref
+        data-te-target="#sidenav-1"
+        aria-controls="#sidenav-1"
+        aria-haspopup="true"
       >
         <span className="sr-only">Toggle Navigation</span>
         <svg
@@ -45,8 +49,21 @@ const Drawer = ({ content }) => {
         <nav
           className="hs-accordion-group p-6 w-full flex flex-col flex-wrap"
           data-hs-accordion-always-open
+        ></nav>
+
+        <nav
+          id="sidenav-1"
+          class="absolute left-0 top-0 z-[1035] h-full w-60 -translate-x-full overflow-hidden bg-white shadow-[0_4px_12px_0_rgba(0,0,0,0.07),_0_2px_4px_rgba(0,0,0,0.05)] data-[te-sidenav-hidden='false']:translate-x-0 dark:bg-zinc-800"
+          data-te-sidenav-init
+          data-te-sidenav-hidden="false"
+          data-te-sidenav-position="absolute"
         >
-          {content}
+          <ul
+            class="relative m-0 list-none px-[0.2rem]"
+            data-te-sidenav-menu-ref
+          >
+            {content}
+          </ul>
         </nav>
       </div>
     </>
